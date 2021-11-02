924c881bca3c7c154e37e637bd49230c4980a1d3
Shohei Yamasaki <sho.yamasaki@gmail.com>
2021-11-02 16:10:27 +0900

    ERC1155, ERC721, ERC20 General Contracts

---
+++ contracts/ERC721General.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+
+contract ERC721General is ERC721 {
+    constructor(string memory name_, string memory symbol_) ERC721(name_, symbol_) {
+    }
+}
